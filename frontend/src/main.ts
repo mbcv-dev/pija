b41a6ae1b4ed15a603afd345546defc712f136d2
mbcv-dev <matheusvieira2006@gmail.com>
2026-06-26 15:30:07 -0300

    feat(front): redesign app shell (topbar, sidebar, bottom-nav) + theme init

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { useThemeStore } from './stores/useThemeStore'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+useThemeStore().init()
 app.mount('#app')
